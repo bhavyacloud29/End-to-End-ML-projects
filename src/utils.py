abb372ce020c6510503c59d5dec45d177eff7ed8
bhavyacloud29 <bhavyachhabra2910@gmail.com>
2026-04-08 23:49:02 +0530

    Added data transformation

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import pickle
+import numpy as np
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+import dill 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+def save_object(file_path,obj):
+    try: 
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        logging.error(f"Error occurred while saving object: {str(e)}")
+        raise CustomException(e, sys) 
